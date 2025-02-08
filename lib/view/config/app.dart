5ef4b91e881b8bc0d968f01a6d7795f1c8c093e6
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-02-08 11:56:53 +0530

    login page

---
+++ lib/view/config/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  static Color app_bg_color = const Color.fromARGB(255, 12, 25, 86);
+  static String app_icon = "assets/icons/app_icon/ic_image.png";
+}
