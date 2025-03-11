5766aedbd6595d655e504ca07d63631e45e90e9d
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-03-11 21:51:07 +0530

    dashboard

--- lib/view/pages/dashboard/dashboard.dart
+++ lib/view/pages/dashboard/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tec_me/view/config/app.dart';
+import 'package:tec_me/view/widgets/drawer.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -51,131 +52,43 @@ class _DashboardPageState extends State<DashboardPage> {
           ),
         ),
       ),
-      drawer: Drawer(
-        backgroundColor: Colors.white,
-        elevation: 20,
-        child: ListView(
-          padding: EdgeInsets.zero,
+      drawer: AppDrawer(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(32.0),
-                ),
-              ),
-              margin: EdgeInsets.only(
-                left: 2,
-                right: 2,
-              ),
-              child: DrawerHeader(
-                decoration: BoxDecoration(color: Color(0xFFD8D8D8)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Good Morning!",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(100),
-                      ),
-                      child: Image.asset(
-                          width: w * 0.2,
-                          height: w * 0.2,
-                          "assets/images/drawer/ic_default_user.png"),
-                    ),
-                    Text(
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      "Roshan Rivishanka",
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                    Text(
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      "roshanrivishanka99@gmail.com",
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                  ],
+            Center(
+              child: Container(
+                width: w * 0.98,
+                height: h * 0.60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12.0),
+                  ),
                 ),
               ),
             ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 200,
-                  ),
-                  ListTile(
-                    trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                    tileColor: Color(0xFFD8D8D8),
-                    onTap: () {},
-                    title: Text(
-                      "Edit Profile",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ListTile(
-                    trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                    tileColor: Color(0xFFD8D8D8),
-                    onTap: () {},
-                    title: Text(
-                      "History",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ListTile(
-                    trailing: Icon(Icons.keyboard_arrow_right_outlined),
-                    tileColor: Color(0xFFD8D8D8),
-                    onTap: () {},
-                    title: Text(
-                      "Add Vehicle",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  ListTile(
-                    trailing: Icon(Icons.logout),
-                    tileColor: Color(0xFFD8D8D8),
-                    onTap: () {},
-                    title: Text(
-                      "Sign Out",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: AppConfig.font_bold_family,
-                      ),
-                    ),
-                  ),
-                ],
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "..Finding Customers..",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                fontFamily: AppConfig.font_bold_family,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/icons/dashboard_icon/ic_find_location.png",
+              width: 50,
+              height: 50,
             ),
           ],
         ),
